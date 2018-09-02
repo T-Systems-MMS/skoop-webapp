c42c781413c5850732b43c65c61a1d7cdde438da
Georg Wittberger <georg.wittberger@gmail.com>
2018-09-02 17:38:03 +0200

    Prepare external configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  authentication: {
+    issuer: document.querySelector('meta[name="myskills-config-authentication-issuer"]').getAttribute('content'),
+    clientId: 'myskills',
+    scope: 'openid profile email',
+    redirectUri: null,
+    silentRefreshRedirectUri: null
+  },
+  serverApiUrl: document.querySelector('meta[name="myskills-config-server-api-url"]').getAttribute('content')
 };
