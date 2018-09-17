3b6ef2fb80f8c5f53290294bb7b184c0cdc1645a
Hadi Mohammadi <rahnemon2011@gmail.com>
2018-09-17 14:29:37 +0200

    Prevent to submit form, if skillName is missing or skillName has less than 3 letters length

--- src/app/my-skills/my-skills-new.component.ts
+++ src/app/my-skills/my-skills-new.component.ts
@@ -23,7 +23,6 @@ export class MySkillsNewComponent implements OnInit, OnDestroy {
   desiredLevel: FormControl = new FormControl(0);
   priority: FormControl = new FormControl(0);
   addedSkillsCount = 0;
-  operationInProgress = false;
   errorMessage: string = null;
   skillSuggestions$: Observable<string[]>;
 
@@ -38,7 +37,6 @@ export class MySkillsNewComponent implements OnInit, OnDestroy {
     this.skillSuggestions$
       .subscribe(() => { }
         , (errorResponse: HttpErrorResponse) => {
-          this.operationInProgress = false;
           this.errorMessage = this.globalErrorHandlerService.createFullMessage(errorResponse);
           // Dirty fix because of: https://github.com/angular/angular/issues/17772
           this.changeDetector.markForCheck();
@@ -50,20 +48,17 @@ export class MySkillsNewComponent implements OnInit, OnDestroy {
   }
 
   addUserSkill(): void {
-    this.operationInProgress = true;
     this.errorMessage = '';
     this.mySkillsService.createCurrentUserSkill(
       this.skillName.value, this.currentLevel.value, this.desiredLevel.value, this.priority.value)
       .subscribe(() => {
         this.addedSkillsCount++;
-        this.operationInProgress = false;
         this.skillName.reset('');
         this.currentLevel.reset(0);
         this.desiredLevel.reset(0);
         this.priority.reset(0);
         document.querySelector<HTMLElement>('#my-skills-new-skill-name').focus();
       }, (errorResponse: HttpErrorResponse) => {
-        this.operationInProgress = false;
         this.errorMessage = this.globalErrorHandlerService.createFullMessage(errorResponse);
         // Dirty fix because of: https://github.com/angular/angular/issues/17772
         this.changeDetector.markForCheck();
