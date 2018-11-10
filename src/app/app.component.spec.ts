e355b5047077ecd02f4a8a7db68887ee3deea74e
Georg Wittberger <georg.wittberger@gmail.com>
2018-11-10 23:32:36 +0100

    Adjust test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,24 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { Observable, of } from 'rxjs';
-
 import { AppMaterialModule } from './app-material.module';
 import { AppComponent } from './app.component';
-import { UserIdentityService } from './shared/user-identity.service';
 import { UserIdentity } from './shared/user-identity';
+import { UserIdentityService } from './shared/user-identity.service';
 
 const userIdentityServiceStub: Partial<UserIdentityService> = {
   getUserIdentity(): Observable<UserIdentity> { return null; }
+};
+
+const testUserIdentity: UserIdentity = {
+  userId: '9a96f28f-8f50-40d9-be1c-605aedd9dfc9',
+  userName: 'tester',
+  firstName: 'Toni',
+  lastName: 'Tester',
+  email: 'toni.tester@myskills.io',
+  roles: ['ROLE_USER']
 };
 
 describe('AppComponent', () => {
@@ -17,12 +26,6 @@ describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
 
   beforeEach(async(() => {
-    spyOn(userIdentityServiceStub, 'getUserIdentity')
-      .and.returnValue(of<UserIdentity>({
-        userId: '9a96f28f-8f50-40d9-be1c-605aedd9dfc9',
-        userName: 'tester',
-        roles: ['ROLE_USER']
-      }));
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -39,27 +42,31 @@ describe('AppComponent', () => {
   }));
 
   beforeEach(() => {
+    spyOn(TestBed.get(UserIdentityService) as UserIdentityService, 'getUserIdentity')
+      .and.returnValue(of<UserIdentity>(testUserIdentity));
+
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create the app', async(() => {
+  it('should create the app', () => {
     expect(component).toBeTruthy();
-  }));
+  });
 
-  it('should fetch an observable for the user identity', async(() => {
+  it('should fetch an observable for the user identity', () => {
     expect(component.userIdentity$).toBeDefined();
-    expect(userIdentityServiceStub.getUserIdentity).toHaveBeenCalled();
-  }));
+    const userIdentityService = TestBed.get(UserIdentityService) as UserIdentityService;
+    expect(userIdentityService.getUserIdentity).toHaveBeenCalled();
+  });
 
-  it('should render the app name in the sidenav toolbar', async(() => {
-    const element = fixture.debugElement.nativeElement;
-    expect(element.querySelector('.sidenav-toolbar').textContent).toContain('MySkills');
-  }));
+  it('should render the app name in the sidenav toolbar', () => {
+    const toolbar = fixture.debugElement.query(By.css('.sidenav-toolbar'));
+    expect(toolbar.nativeElement.textContent).toContain('MySkills');
+  });
 
-  it('should render the user name in the toolbar', async(() => {
-    const element = fixture.debugElement.nativeElement;
-    expect(element.querySelector('.page-header-username').textContent).toContain('tester');
-  }));
+  it('should render the user name in the toolbar', () => {
+    const username = fixture.debugElement.query(By.css('.contentToolbar__username'));
+    expect(username.nativeElement.textContent).toBe('tester');
+  });
 });
