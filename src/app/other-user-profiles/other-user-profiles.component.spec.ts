ebfd9d7a8179c28c7565a3380d1705697af45b0b
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-01-21 15:10:25 +0100

    tests

--- src/app/other-user-profiles/other-user-profiles.component.spec.ts
+++ src/app/other-user-profiles/other-user-profiles.component.spec.ts
@@ -1,6 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OtherUserProfilesComponent } from './other-user-profiles.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../app-material.module';
+import { GlobalErrorHandlerService } from '../error/global-error-handler.service';
+import { of } from 'rxjs';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/user';
+
+const permissionOwners: User[] = [
+  {
+    id: 'e6b808eb-b6bd-447d-8dce-3e0d66b17759',
+    userName: 'fius',
+    firstName: 'first',
+    lastName: 'user',
+    email: 'first.user@myskills.io',
+    coach: true,
+  },
+  {
+    id: '666808eb-b6bd-447d-8dce-3e0d66b17666',
+    userName: 'seus',
+    firstName: 'second',
+    lastName: 'user',
+    email: 'second.user@myskills.io',
+    coach: true,
+  }
+];
 
 describe('OtherUserProfilesComponent', () => {
   let component: OtherUserProfilesComponent;
@@ -8,9 +36,24 @@ describe('OtherUserProfilesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OtherUserProfilesComponent ]
+      imports: [
+        BrowserAnimationsModule,
+        LayoutModule,
+        FlexLayoutModule,
+        ReactiveFormsModule,
+        AppMaterialModule
+      ],
+      declarations: [OtherUserProfilesComponent],
+      providers: [
+        GlobalErrorHandlerService,
+        {
+          provide: UsersService, useValue: jasmine.createSpyObj('usersService', {
+            'getPermissionOwners': of<User[]>(permissionOwners)
+          })
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +64,9 @@ describe('OtherUserProfilesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load the list of users who granted permission', () => {
+    expect(component.permissionOwners).toEqual(permissionOwners);
   });
 });
