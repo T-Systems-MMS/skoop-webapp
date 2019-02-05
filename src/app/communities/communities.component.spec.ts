e2960d4576b5785d1bda59aa8d2c0c3acd9382e0
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-02-05 14:37:11 +0100

    displaying list of communities

--- src/app/communities/communities.component.spec.ts
+++ src/app/communities/communities.component.spec.ts
@@ -1,6 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CommunitiesComponent } from './communities.component';
+import { AppMaterialModule } from '../app-material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
+import { of } from 'rxjs';
+import { GlobalErrorHandlerService } from '../error/global-error-handler.service';
+import { CommunitiesService } from './communities.service';
+import { Community } from './community';
+
+const communities = [
+  {
+    id: '123',
+    title: 'group1',
+    description: 'super group description'
+  },
+  {
+    id: '123456',
+    title: 'group2',
+    description: 'other group'
+  }
+];
 
 describe('CommunitiesComponent', () => {
   let component: CommunitiesComponent;
@@ -8,9 +28,23 @@ describe('CommunitiesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CommunitiesComponent ]
+      imports: [
+        AppMaterialModule,
+        BrowserAnimationsModule,
+        MatMomentDateModule
+      ],
+      declarations: [CommunitiesComponent],
+      providers: [
+        {
+          provide: CommunitiesService, useValue: jasmine.createSpyObj('communityService', {
+            'getCommunities': of<Community[]>(communities),
+            'deleteCommunity': of<void>()
+          })
+        },
+        GlobalErrorHandlerService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/communities/communities.component.ts
+++ src/app/communities/communities.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommunitiesService } from './communities.service';
+import { Observable, of } from 'rxjs';
+import { Community } from './community';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
+import { GlobalErrorHandlerService } from '../error/global-error-handler.service';
 
 @Component({
   selector: 'app-communities',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunitiesComponent implements OnInit {
 
-  constructor() { }
+  communities$: Observable<Community[]> = of([]);
+  errorMessage: string = null;
+
+  constructor(private communityService: CommunitiesService,
+              private changeDetector: ChangeDetectorRef,
+              private globalErrorHandlerService: GlobalErrorHandlerService) {
+  }
 
   ngOnInit() {
+    this.loadCommunities();
+  }
+
+  private loadCommunities() {
+    this.communities$ = this.communityService.getCommunities()
+      .pipe(
+        catchError((err: HttpErrorResponse, caught: Observable<Community[]>) => {
+          this.errorMessage = this.globalErrorHandlerService.createFullMessage(err);
+          return of([]);
+        })
+      );
+    // Dirty fix because of: https://github.com/angular/angular/issues/17772
+    this.changeDetector.markForCheck();
   }
 
 }
