e53f09ca26d76f87fc014b0f2f3b1f2661b17ee9
Dmitry Lebedko <lebedko.dmitrii@gmail.com>
2019-02-13 15:26:40 +0300

    Community edit / delete buttons are not displayed for all users but community managers. (#46)

--- src/app/communities/communities.component.spec.ts
+++ src/app/communities/communities.component.spec.ts
@@ -15,8 +15,9 @@ import { CommunityType } from './community-type.enum';
 import { UserIdentityService } from '../shared/user-identity.service';
 import { UserIdentity } from '../shared/user-identity';
 import { CommunityResponse } from './community-response';
+import { User } from '../users/user';
 
-const communities = [
+const communities: CommunityResponse[] = [
   {
     id: '123',
     title: 'group1',
@@ -30,13 +31,37 @@ const communities = [
       {
         name: 'stackoveflow',
         href: 'https://stackoverflow.com/'
-      }]
+      }],
+    members: [
+      {
+        id: 'e6b808eb-b6bd-447d-8dce-3e0d66b17759',
+        userName: 'tester'
+      } as User
+    ],
+    managers: [
+      {
+        id: 'e6b808eb-b6bd-447d-8dce-3e0d66b17759',
+        userName: 'tester'
+      } as User
+    ]
   },
   {
     id: '123456',
     title: 'group2',
     description: 'other group',
     type: CommunityType.OPENED,
+    members: [
+      {
+        id: 'c6d8badc-0f80-4d20-b436-0e7e871f97f5',
+        userName: 'anotherTester'
+      } as User
+    ],
+    managers: [
+      {
+        id: 'c6d8badc-0f80-4d20-b436-0e7e871f97f5',
+        userName: 'anotherTester'
+      } as User
+    ]
   }
 ];
 
@@ -118,6 +143,16 @@ describe('CommunitiesComponent', () => {
     component.joinCommunity({id: 'd11235de-f13e-4fd6-b5d6-9c4c4e18aa4f'} as CommunityResponse);
     fixture.detectChanges();
     expect(component.isCommunityJoined({id: 'd11235de-f13e-4fd6-b5d6-9c4c4e18aa4f'} as CommunityResponse)).toBe(true);
+  }));
+
+  it('should check if the user is manager of a community', fakeAsync(() => {
+    fixture.detectChanges();
+    expect(component.isCommunityManager(communities[0])).toBe(true);
+  }));
+
+  it('should check if the user is not manager of a community', fakeAsync(() => {
+    fixture.detectChanges();
+    expect(component.isCommunityManager(communities[1])).toBe(false);
   }));
 
 });
