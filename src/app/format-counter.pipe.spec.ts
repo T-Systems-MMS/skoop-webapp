36961f43e8bc499d11f0cf8a7a7978a25ae24c96
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-02-26 14:40:19 +0100

    fix NPE

--- src/app/format-counter.pipe.spec.ts
+++ src/app/format-counter.pipe.spec.ts
@@ -7,6 +7,10 @@ describe('FormatCounterPipe', () => {
     expect(pipe).toBeTruthy();
   });
 
+  it('should return 0 if value is null', () => {
+    expect(pipe.transform(null)).toBe('0');
+  });
+
   it('should return an initial value if value <= 99', () => {
     expect(pipe.transform(99)).toBe('99');
   });
--- src/app/format-counter.pipe.ts
+++ src/app/format-counter.pipe.ts
@@ -6,6 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FormatCounterPipe implements PipeTransform {
 
   transform(value: number, args?: any): string {
+    if (value === null) {
+      return '0';
+    }
+
     return value <= 99 ? value.toString() : '99+';
   }
 
