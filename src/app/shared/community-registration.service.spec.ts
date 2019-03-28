c9ca5577de0bc580e00ec0985ac686a59a2d8f9c
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-03-28 07:47:54 +0100

    test for registration service

--- src/app/shared/community-registration.service.spec.ts
+++ src/app/shared/community-registration.service.spec.ts
@@ -4,6 +4,7 @@ import { CommunityRegistrationService } from './community-registration.service';
 import { CommunityUserRegistrationResponse } from './community-user-registration-response';
 import { environment } from '../../environments/environment';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { CommunityUserRegistration } from './community-user-registration';
 
 describe('CommunityRegistrationService', () => {
   let httpTestingController: HttpTestingController;
@@ -70,5 +71,43 @@ describe('CommunityRegistrationService', () => {
     expect(request.request.body).toEqual({userIds: userIds});
 
     request.flush(communityUserRegistrationResponse);
+  }));
+
+  it('should send an update registration request', async(() => {
+    const communityId = 'e6b808eb-b6bd-447d-8dce-3e0d66b17759';
+
+    const updateResponse: CommunityUserRegistrationResponse = {
+      id: '12345',
+      user: {
+        id: '2736a204-f3ab-4b65-8568-a1c8ce1db8ab',
+        userName: 'testing',
+        firstName: 'Tina',
+        lastName: 'Testing',
+        email: 'tina.testing@myskills.io',
+        coach: false,
+      },
+      approvedByUser: false,
+      approvedByCommunity: true
+    };
+
+    const userRegistrationRequest: CommunityUserRegistration = {
+      id: '12345',
+      approvedByUser: false,
+      approvedByCommunity: true
+    };
+
+    communityRegistrationService.updateRegistration(communityId, userRegistrationRequest).subscribe(community => {
+      expect(community).toEqual(updateResponse);
+    });
+
+    const request = httpTestingController.expectOne({
+      method: 'PUT',
+      url: `${environment.serverApiUrl}/communities/${communityId}/user-registrations/${userRegistrationRequest.id}`
+    });
+
+    expect(request.request.responseType).toEqual('json');
+    expect(request.request.body).toEqual(userRegistrationRequest);
+
+    request.flush(updateResponse);
   }));
 });
