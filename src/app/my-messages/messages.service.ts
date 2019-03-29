b479665f4603470b4e8abf8e72a956d6e944659f
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-03-29 17:42:16 +0100

    visibility of accept/decline buttons

--- src/app/my-messages/messages.service.ts
+++ src/app/my-messages/messages.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { UserIdentityService } from '../shared/user-identity.service';
 import { switchMap } from 'rxjs/operators';
 import { AbstractNotification } from './abstract-notification';
 import { Util } from '../util/util';
@@ -14,18 +13,12 @@ export class MessagesService {
 
   private messagesUrlPattern = `${environment.serverApiUrl}/users/{userId}/notifications`;
 
-  constructor(private httpClient: HttpClient,
-              private userIdentityService: UserIdentityService) {
+  constructor(private httpClient: HttpClient) {
   }
 
-  getUserNotifications<T extends AbstractNotification>(): Observable<T[]> {
-    return this.userIdentityService.getUserIdentity()
-      .pipe(
-        switchMap(userIdentity =>
-          this.httpClient.get<T[]>(this.messagesUrlPattern.replace('{userId}', userIdentity.userId))
-            .pipe(switchMap(notifications =>
-              of(notifications.map(item => Util.createNotificationInstance(item)))
-            ))
-        ));
+  getUserNotifications<T extends AbstractNotification>(userId: string): Observable<T[]> {
+    return this.httpClient.get<T[]>(this.messagesUrlPattern.replace('{userId}', userId))
+      .pipe(switchMap(notifications =>
+        of(notifications.map(item => Util.createNotificationInstance(item)))));
   }
 }
