0bafba7705f5f7873f770a9c03ddf4980de16330
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-04-01 15:56:49 +0200

    adjust the structure of MemberLeftCommunityNotification

--- src/app/my-messages/member-left-community-notification.ts
+++ src/app/my-messages/member-left-community-notification.ts
@@ -1,8 +1,10 @@
 import { AbstractNotification } from './abstract-notification';
 import { Community } from '../communities/community';
+import { User } from '../users/user';
 
 export class MemberLeftCommunityNotification extends AbstractNotification {
   community: Community;
+  user: User;
 
   getTypeText(): string {
     return 'A member leaving a community';
