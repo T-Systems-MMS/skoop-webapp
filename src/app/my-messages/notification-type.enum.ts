a0fed99aca0b9c2d046a629cafb189bec91bb59a
Svetlana Ivanova <svetlana.ivanova.1@ya.ru>
2019-04-08 16:40:04 +0200

    [SKOOP-119] CommunityChangedNotification (#76)
    * CommunityChangedNotification
    tests
    * fix test
    * fix sonar issue

--- src/app/my-messages/notification-type.enum.ts
+++ src/app/my-messages/notification-type.enum.ts
@@ -3,7 +3,7 @@ export enum NotificationType {
   REQUEST_TO_JOIN_COMMUNITY = 'RequestToJoinCommunityNotification',
   MEMBER_LEFT_COMMUNITY = 'UserLeftCommunityNotification',
   COMMUNITY_ROLE_CHANGED = 'CommunityUserRoleChangedNotification',
-  COMMUNITY_CHANGED = 'COMMUNITY_CHANGED',
+  COMMUNITY_CHANGED = 'CommunityChangedNotification',
   MEMBER_KICKED_OUT_OF_COMMUNITY = 'UserKickedOutFromCommunityNotification',
   COMMUNITY_DELETED = 'CommunityDeletedNotification',
   ACCEPTANCE_TO_COMMUNITY = 'AcceptanceToCommunityNotification'
