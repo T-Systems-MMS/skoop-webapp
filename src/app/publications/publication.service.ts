04e00c8dc5497d04a08b04c2906da0e6c9c95289
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-04-19 09:50:44 +0200

    PublicationsNewComponent

--- src/app/publications/publication.service.ts
+++ src/app/publications/publication.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PublicationResponse } from './publication-response';
+import { PublicationRequest } from './publication-request';
+import { switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { UserIdentityService } from '../shared/user-identity.service';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PublicationService {
 
-  constructor() { }
+  private publicationsUrlPattern = `${environment.serverApiUrl}/users/{userId}/publications`;
+  private publicationUrlPattern = `${environment.serverApiUrl}/users/{userId}/publications/{publicationId}`;
+
+  constructor(private httpClient: HttpClient,
+              private userIdentityService: UserIdentityService) {
+  }
+
+  createPublication(publication: PublicationRequest): Observable<PublicationResponse> {
+    return this.userIdentityService.getUserIdentity()
+      .pipe(switchMap(userIdentity =>
+        this.httpClient.post<PublicationResponse>(this.publicationsUrlPattern.replace('{userId}', userIdentity.userId), publication)));
+  }
 }
--- src/app/publications/publications-new.component.ts
+++ src/app/publications/publications-new.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { GlobalErrorHandlerService } from '../error/global-error-handler.service';
+import { MatBottomSheetRef } from '@angular/material';
+import { PublicationService } from './publication.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { PublicationRequest } from './publication-request';
 
 @Component({
   selector: 'app-publications-new',
@@ -7,9 +13,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PublicationsNewComponent implements OnInit {
 
-  constructor() { }
+  publicationForm: FormGroup;
+  errorMessage: string = null;
+  maxDate: Date = new Date();
+
+  constructor(private publicationService: PublicationService,
+              private formBuilder: FormBuilder,
+              private changeDetector: ChangeDetectorRef,
+              private globalErrorHandlerService: GlobalErrorHandlerService,
+              private bottomSheet: MatBottomSheetRef) {
+  }
 
   ngOnInit() {
+    this.publicationForm = this.formBuilder.group({
+      title: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required),
+      publisher: new FormControl('', Validators.required),
+      link: new FormControl('', Validators.required),
+      skills: new FormControl([])
+    });
+  }
+
+  addPublication() {
+    this.publicationService.createPublication(this.getPublicationData())
+      .subscribe((data) => {
+        this.publicationForm.reset();
+        this.bottomSheet.dismiss(data);
+      }, (errorResponse: HttpErrorResponse) => {
+        this.handleErrorResponse(errorResponse);
+      });
+  }
+
+  close() {
+    this.bottomSheet.dismiss();
+  }
+
+  private getPublicationData(): PublicationRequest {
+    return {
+      title: this.publicationForm.get('title').value,
+      date: this.publicationForm.get('date').value,
+      publisher: this.publicationForm.get('publisher').value,
+      link: this.publicationForm.get('link').value,
+      skills: this.skillsArray || []
+    } as PublicationRequest;
+  }
+
+  private handleErrorResponse(errorResponse: HttpErrorResponse) {
+    this.errorMessage = this.globalErrorHandlerService.createFullMessage(errorResponse);
+    // Dirty fix because of: https://github.com/angular/angular/issues/17772
+    this.changeDetector.markForCheck();
+  }
+
+  get skillsArray(): string[] {
+    return this.publicationForm.get('skills').value;
   }
 
 }
