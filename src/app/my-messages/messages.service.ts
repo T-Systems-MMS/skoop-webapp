37e0b648f87bc3073a44ca9de185ccb660c15673
Svetlana Ivanova <svetlana.ivanova.1@ya.ru>
2019-04-22 15:56:54 +0300

    [SKOOP-139] Notification counter (#87)
    * Message counter is fetched with new API.
    * reworked NotificationCounterService.getCount method
    tests

--- src/app/my-messages/messages.service.ts
+++ src/app/my-messages/messages.service.ts
@@ -13,6 +13,7 @@ export class MessagesService {
 
   private messagesUrlPattern = `${environment.serverApiUrl}/users/{userId}/notifications`;
   private messageUrlPattern = `${environment.serverApiUrl}/notifications/{notificationId}`;
+  private messageCounterUrlPatter = `${environment.serverApiUrl}/users/{userId}/notification-counter`;
 
   constructor(private httpClient: HttpClient) {
   }
@@ -21,6 +22,10 @@ export class MessagesService {
     return this.httpClient.get<T[]>(this.messagesUrlPattern.replace('{userId}', userId))
       .pipe(switchMap(notifications =>
         of(notifications.map(item => Util.createNotificationInstance(item)))));
+  }
+
+  getUserNotificationCounter(userId: string): Observable<number> {
+    return this.httpClient.get<number>(this.messageCounterUrlPatter.replace('{userId}', userId));
   }
 
   delete(notificationId: string): Observable<void> {
--- src/app/shared/notification-counter.service.ts
+++ src/app/shared/notification-counter.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
+import { MessagesService } from '../my-messages/messages.service';
+import { UserIdentityService } from './user-identity.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,8 @@ export class NotificationCounterService {
   private notificationCount = 0;
   private subject = new Subject<number>();
 
-  constructor() {
-  }
-
-  setCount(count: number) {
-    this.notificationCount = count;
-    this.subject.next(count);
+  constructor(private messagesService: MessagesService,
+              private userIdentityService: UserIdentityService) {
   }
 
   decrementCount() {
@@ -23,6 +21,14 @@ export class NotificationCounterService {
   }
 
   getCount(): Observable<number> {
+    this.userIdentityService.getUserIdentity().subscribe(userIdentity => {
+      this.messagesService.getUserNotificationCounter(userIdentity.userId)
+        .subscribe(counter => {
+          this.notificationCount = counter;
+          this.subject.next(counter);
+        });
+    });
+
     return this.subject.asObservable();
   }
 }
