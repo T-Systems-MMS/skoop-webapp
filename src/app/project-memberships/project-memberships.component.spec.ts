67a595a86eaf9d51a6cd253f8cdf6ba8b4c7c38e
Svetlana Ivanova <svetlana.ivanova.1@ya.ru>
2019-05-29 13:26:09 +0300

    [SKOOP-183] Button to approve the project (#102)
    * approve button
    * request to approve project
    * tests

--- src/app/project-memberships/project-memberships.component.spec.ts
+++ src/app/project-memberships/project-memberships.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
 import { ProjectMembershipsComponent } from './project-memberships.component';
 import { AppMaterialModule } from '../app-material.module';
@@ -12,6 +12,7 @@ import { UsersService } from '../users/users.service';
 import * as moment from 'moment';
 import { UserProjectCardComponent } from '../shared/user-project-card/user-project-card.component';
 import { GlobalErrorHandlerService } from '../error/global-error-handler.service';
+import { UpdateUserProjectRequest } from '../user-projects/update-user-project-request';
 
 const userProjects = [
   {
@@ -46,7 +47,8 @@ const userProjects = [
         description: 'Java programming language.',
         skillGroups: null
       }
-    ]
+    ],
+    approved: false
   }
 ];
 
@@ -76,7 +78,10 @@ describe('ProjectMembershipsComponent', () => {
       providers: [
         {
           provide: UserProjectsService,
-          useValue: jasmine.createSpyObj('userProjectService', {'getUserProjects': of(userProjects)})
+          useValue: jasmine.createSpyObj('userProjectService', {
+            'getUserProjects': of(userProjects),
+            'updateUserProject': of(userProjects[0])
+          })
         },
         {
           provide: UsersService,
@@ -97,4 +102,21 @@ describe('ProjectMembershipsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call UserProjectsService.updateUserProject with expected parameters', fakeAsync(() => {
+    const expectedResponse: UpdateUserProjectRequest = {
+      role: userProjects[0].role,
+      skills: ['Java'],
+      tasks: userProjects[0].tasks,
+      startDate: userProjects[0].startDate,
+      endDate: userProjects[0].endDate,
+      approved: true
+    };
+
+    component.onApprove(userProjects[0]);
+    fixture.detectChanges();
+
+    const userProjectsService: UserProjectsService = TestBed.get(UserProjectsService);
+    expect(userProjectsService.updateUserProject).toHaveBeenCalledWith(user.id, userProjects[0].project.id, expectedResponse);
+  }));
 });
--- src/app/user-projects/update-user-project-request.ts
+++ src/app/user-projects/update-user-project-request.ts
@@ -6,4 +6,5 @@ export interface UpdateUserProjectRequest {
   startDate: Moment;
   endDate: Moment;
   skills?: string[];
+  approved?: boolean;
 }
