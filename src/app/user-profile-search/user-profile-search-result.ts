39f0eee737cd5b856d527ccdf1212d889c4742fd
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-06-28 15:57:42 +0200

    add userSkills to the UserProfileSearchResult

--- src/app/user-profile-search/user-profile-search-result.ts
+++ src/app/user-profile-search/user-profile-search-result.ts
@@ -1,5 +1,7 @@
 import { User } from '../users/user';
+import { UserSkill } from '../user-skills/user-skill';
 
 export interface UserProfileSearchResult extends User {
   manager?: User;
+  skills?: UserSkill[];
 }
