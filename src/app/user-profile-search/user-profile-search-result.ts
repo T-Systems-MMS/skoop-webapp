e4cd8ddc0d0b417712b53870536aabd8baa88145
Svetlana Ivanova <Svetlana.Ivanova@t-systems.ru>
2019-06-28 15:47:02 +0200

    remove userSkills from the UserProfileSearchResult

--- src/app/user-profile-search/user-profile-search-result.ts
+++ src/app/user-profile-search/user-profile-search-result.ts
@@ -1,7 +1,5 @@
 import { User } from '../users/user';
-import { UserSkill } from '../user-skills/user-skill';
 
 export interface UserProfileSearchResult extends User {
   manager?: User;
-  skills?: UserSkill[];
 }
